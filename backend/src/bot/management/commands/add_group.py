7c4171e88d11a8818de479e89c86d83b5d0195e7
Oleg Serikov <srkvoa@gmail.com>
2021-10-18 07:40:45 +0300

    Update add_group.py

--- backend/src/bot/management/commands/add_group.py
+++ backend/src/bot/management/commands/add_group.py
@@ -7,10 +7,11 @@ class Command(BaseCommand):
     help = 'Add a new group polling with telegram bot'
 
     def add_arguments(self, parser):
-        parser.add_argument('group_name', type=str, required=True)
-        parser.add_argument('group_id', type=int, required=True)
+        parser.add_argument('group_name', type=str)
+        parser.add_argument('group_id', type=int)
     
     def handle(self, *args, **options):
+        assert "group_id" in options and "group_name" in options, "both group_id and group_name arguments should be provided"
         group = models.Groups.objects.create(id=options['group_id'], name=options['group_name'])
         group.save()
         self.stdout.write(self.style.SUCCESS(f'{group} created'))
